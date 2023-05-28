9edf2ea5603ce3ec152048dd554a599088b53754
FeliciaBrihan <felicia.brihan@gmail.com>
2023-05-28 20:24:47 +0300

    server.ts add middleware(cors,json)

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,8 +1,12 @@
 import { createServer } from 'http';
-import express, { Response, Request } from 'express';
+import express, { json, Response, Request } from 'express';
+import cors from 'cors';
 
 const app = express();
 const httpServer = createServer(app);
+
+app.use(cors());
+app.use(json());
 
 app.get('/', (req: Request, res: Response) => {
 	return res.send('API');
