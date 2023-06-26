54a407aa93b3b14f0883c5c643395f368817ba13
FeliciaBrihan <felicia.brihan@gmail.com>
2023-06-26 22:22:09 +0300

    create Model Role

--- packages/backend/src/init/initDatabaseModels.ts
+++ packages/backend/src/init/initDatabaseModels.ts
@@ -7,9 +7,11 @@ export async function initDatabaseModels(sequelize: Sequelize) {
 	async function addModels(sequelize: Sequelize) {
 		const { getModelBook } = await import('src/modules/book/models');
 		const { getModelUser } = await import('src/modules/user/models');
+		const { getModelRole } = await import('src/modules/role/models');
 
 		getModelBook(sequelize);
 		getModelUser(sequelize);
+		getModelRole(sequelize);
 
 		await sequelize.sync();
 	}
---
+++ packages/backend/src/modules/role/routes/index.ts
@@ -0,0 +1,7 @@
+export const routeName = 'role';
+
+import { Router } from 'express';
+
+const router = Router();
+
+export const routes = router;
